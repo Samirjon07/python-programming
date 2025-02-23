99460c9b8f9fcd495d39d795aa969f4292219451
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-02-23 19:37:11 +0500

    Delete Problems directory

--- Problems/04.py
+++
@@ -1,8 +0,0 @@
-input_sequence = input("Enter a sequence of comma-separated numbers: ")
-
-number_list = input_sequence.split(',')
-
-number_tuple = tuple(number_list)
-
-print(number_list)
-print(number_tuple)
