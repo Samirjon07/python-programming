22b344979dde50eef5343e93d73e1be8df6bbd27
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-02-26 00:52:11 +0500

    Addition and subtraction

---
+++ Assignments/Matrix manipulation using numpy/matrices_addition.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+mat1=np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+mat2=np.array([[0,1,2],[3,4,5],[6,7,8]])
+
+
+#First method .zeroes will create matrix of zeros where after all values there is dot
+add=np.zeros((3,3))
+
+if (mat1.shape[1]==mat2.shape[1]) & (mat1.shape[0]==mat2.shape[0]):
+    for i in range(mat1.shape[1]):
+        for j in range(mat1.shape[0]):
+            add[i,j]=mat1[i,j]+mat2[i,j]
+print("\n",add)            
+
+
+
+#Second method .full will create 3*3 matrix where  all values are 0 (no dot)
+add=np.full((3,3),0)
+
+if (mat1.shape[1]==mat2.shape[1]) & (mat1.shape[0]==mat2.shape[0]):
+    for i in range(mat1.shape[1]):
+        for j in range(mat1.shape[0]):
+            add[i,j]=mat1[i,j]+mat2[i,j]
+print("\n",add)
