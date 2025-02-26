6f6e529704a9e68619a0245a63edc17a723bd99d
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-02-26 09:30:48 +0500

    commit

---
+++ Classwork practice/Matplotlip/matlpotlip manipulation.py
@@ -0,0 +1,45 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+'''
+xpoints=np.array([0,8])
+ypoints=np.array([0,250])
+
+plt.plot(xpoints,ypoints) # plot method takes two parameters xpoints and ypoints
+plt.show()#it will show the graph
+
+
+
+xpoints=np.array([0,8])
+ypoints=np.array([0,250])
+
+plt.plot(xpoints,ypoints,'o') # "o" will show the points in the graph only
+plt.show()
+
+
+
+xpoints=np.array([0,5,10,13,15])
+ypoints=np.array([0,5,0,10,5])
+
+plt.plot(xpoints,ypoints) 
+plt.show()
+'''
+
+xpoints=np.array([0,2,2,4,10,14])
+ypoints=np.array([0,0,2,2,10,10])
+
+plt.plot(xpoints,ypoints) 
+plt.show()
+
+
+#markers
+xpoints=np.array([0,2,2,4,10,14])
+ypoints=np.array([0,0,2,2,10,10])
+
+plt.plot(xpoints,ypoints,marker='4') # in the graph it will show the points too
+plt.show()
+
+
+
+
---
+++ Classwork practice/Numpy/Random_data_dist.py
@@ -0,0 +1,15 @@
+from numpy import random 
+
+x=random.choice([3,5,7,9],size=(100))
+print("\n",x)#it will generate 100 number which are
+
+
+x=random.choice([3,5,7,9],size=(100),p=[0.1,0.3,0.4,0.2])
+print("\n",x)#p shows probability: 0.1 means 10 %
+
+
+x=random.choice([3,5,7,9],size=(2,3),p=[0.1,0.3,0.4,0.2])
+print("\n",x)# we have creats 2*3 matrix
+
+
+#shuffling arrays
