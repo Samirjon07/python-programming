88a5235ce385cda0af845102bbd0ee1dcef063a2
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-03-07 15:21:06 +0500

    Child class

---
+++ Extra practice/OOP/3)Class_with_child.py
@@ -0,0 +1,29 @@
+"""
+Create a child class Bus that will inherit all of the variables and methods of the Vehicle class
+Given:
+
+class Vehicle:
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+Create a Bus object that will inherit all of the variables and methods of the parent Vehicle class and display it.
+
+Expected Output:
+
+Vehicle Name: School Volvo Speed: 180 Mileage: 12
+"""
+class Vehicle:
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Vehicle):
+    pass
+
+school_bus = Bus("School Volvo", 180, 12)
+print("Vehicle Name:", school_bus.name, "Speed:", school_bus.max_speed, "Mileage:", school_bus.mileage)
