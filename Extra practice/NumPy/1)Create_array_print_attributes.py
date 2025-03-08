c3d4e3ae738f7d6be6c26435729b984a1571a9dd
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-03-08 23:44:25 +0500

    Numpy

---
+++ Extra practice/NumPy/1)Create_array_print_attributes.py
@@ -0,0 +1,22 @@
+'''
+Create a 4X2 integer array and Prints its attributes
+Note: The element must be a type of unsigned int16. And print the following Attributes: –
+
+The shape of an array.
+Array dimensions.
+The Length of each element of the array in bytes.
+Expected Output:
+
+Printing Array
+
+[[64392 31655]
+ [32579     0]
+ [49248   462]
+ [    0     0]]
+
+Printing NumPy array Attributes
+
+Array Shape is:  (4, 2)
+Array dimensions are  2
+Length of each element of array in bytes is  2
+'''
