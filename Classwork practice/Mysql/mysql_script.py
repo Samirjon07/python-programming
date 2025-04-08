46e80ef22d4402c07979764403595f2ed7112bcf
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-04-09 02:19:59 +0500

    MySQL

--- Classwork practice/Mysql/mysql_script.py
+++ Classwork practice/Mysql/mysql_script.py
@@ -4,14 +4,19 @@ from mysql.connector import Error
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="1234567890",
-    database="siut"
+    password="Samirjon07",
+    database="python"
 )
 
 if conn.is_connected():
     print("Connection established")
 
 mycursor = conn.cursor()
-mycursor.execute("SELECT * FROM course")
+mycursor.execute("INSERT INTO grades (id) VALUES('15')")
+conn.commit()
+
+mycursor.execute("SELECT * FROM grades")    
+
+
 myresult = mycursor.fetchall()
 print(myresult)
--- Classwork practice/Numpy/Random_data_dist.py
+++ Classwork practice/Numpy/Random_data_dist.py
@@ -12,4 +12,3 @@ x=random.choice([3,5,7,9],size=(2,3),p=[0.1,0.3,0.4,0.2])
 print("\n",x)# we have creats 2*3 matrix
 
 
-#shuffling arrays
