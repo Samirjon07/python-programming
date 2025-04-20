7448f095149cf4606d7759bf2629e923aeb3cd8f
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-04-20 13:26:52 +0500

    Kivy

--- Classwork practice/Kivy/kivymanipulation3.py
+++ Classwork practice/Kivy/kivymanipulation3.py
@@ -3,7 +3,7 @@ from kivymd.app import MDApp
 from kivy.uix.relativelayout import RelativeLayout
 from kivymd.uix.label import MDLabel,MDIcon 
 from kivymd.uix.button import MDRaisedButton,MDRoundFlatButton 
-from kivy.uix.screen import Screen
-from kivy.uix.dialog import MDDialog
-from kivy.uix.list import MDList,OneLineListItem
+from kivymd.uix.screen import Screen
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.list import MDList,OneLineListItem
 from kivy.uix.scrollview import ScrollView
